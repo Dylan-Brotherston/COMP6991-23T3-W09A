e1554919598d1122124225a95d616d199622e1cc
Eric Paul <ericcpaul00@gmail.com>
2023-09-27 10:32:48 +1000

    wk3

--- ws03/src/main.rs
+++ ws03/src/main.rs
@@ -1,12 +1,14 @@
-use termgame::{Controller, CharChunkMap, GameSettings, Game, GameEvent, SimpleEvent, KeyCode, run_game};
+use termgame::{
+    run_game, CharChunkMap, Controller, Game, GameColor, GameEvent, GameSettings, GameStyle,
+    KeyCode, SimpleEvent, StyledCharacter,
+};
 
 use std::error::Error;
 use std::time::Duration;
 
-
 /// This is a single "buffer".
 struct Buffer {
-    text: String
+    text: String,
 }
 
 impl Buffer {
@@ -16,7 +18,7 @@ impl Buffer {
     /// ```
     fn new() -> Buffer {
         Buffer {
-            text: String::new()
+            text: String::new(),
         }
     }
 
@@ -64,7 +66,6 @@ impl Buffer {
     // fn example_ref(&self) -> i32 {
     //     todo!()
     // }
-
 }
 
 /// This struct implements all the
@@ -73,64 +74,87 @@ impl Buffer {
 /// something should interact with the terminal.
 struct BufferEditor {
     buffer: Buffer,
+    cursor_visible: bool,
+}
+
+impl BufferEditor {
+    fn get_cursor(&self) -> StyledCharacter {
+        StyledCharacter::new(' ').style(GameStyle::new().background_color(Some(GameColor::Gray)))
+    }
+
+    fn get_cursor_location(&self) -> (i32, i32) {
+        let lines: Vec<&str> = self.buffer.text.lines().collect();
+        let y = std::cmp::max(lines.len() as i32 - 1, 0);
+        let x = lines
+            .last()
+            .map(|line| line.chars().count() as i32)
+            .unwrap_or(0); // x is the length of the last line
+        (x, y)
+    }
+
+    fn toggle_cursor(&mut self) {
+        self.cursor_visible = !self.cursor_visible;
+    }
 }
 
 impl Controller for BufferEditor {
     /// This gets run once, you can probably ignore it.
-    fn on_start(&mut self, _game: &mut Game) {
-    }
+    fn on_start(&mut self, _game: &mut Game) {}
 
     /// Any time there's a keypress, you'll get this
     /// function called.
     fn on_event(&mut self, game: &mut Game, event: GameEvent) {
         match event.into() {
-            SimpleEvent::Just(KeyCode::Char(c)) => {
-                self.buffer.push_char(c)
-            },
-            SimpleEvent::Just(KeyCode::Enter) => {
-                self.buffer.push_char('\n')
-            },
-            SimpleEvent::Just(KeyCode::Backspace) => {
-                self.buffer.pop_char()
-            },
+            SimpleEvent::Just(KeyCode::Char(c)) => self.buffer.push_char(c),
+            SimpleEvent::Just(KeyCode::Enter) => self.buffer.push_char('\n'),
+            SimpleEvent::Just(KeyCode::Backspace) => self.buffer.pop_char(),
             SimpleEvent::Just(KeyCode::Esc) => {
                 game.end_game();
-            },
+            }
             SimpleEvent::Just(KeyCode::Up) => {
                 let mut viewport = game.get_viewport();
                 if viewport.y > 0 {
                     viewport.y -= 1;
                 }
                 game.set_viewport(viewport)
-            },
+            }
             SimpleEvent::Just(KeyCode::Down) => {
                 let mut viewport = game.get_viewport();
                 viewport.y += 1;
                 game.set_viewport(viewport)
-            },
+            }
             _ => {}
         }
         let mut chunkmap = CharChunkMap::new();
         self.buffer.chunkmap_from_textarea(&mut chunkmap);
         game.swap_chunkmap(&mut chunkmap);
-
     }
 
     /// This function gets called regularly, so you can use it
     /// for logic that's independent of key-presses like
     /// implementing a "mouse".
-    fn on_tick(&mut self, _game: &mut Game) {}
+    fn on_tick(&mut self, _game: &mut Game) {
+        let (x, y) = self.get_cursor_location();
+        self.toggle_cursor();
+
+        if self.cursor_visible {
+            _game.set_screen_char(x, y, Some(self.get_cursor()));
+        } else {
+            _game.set_screen_char(x, y, None);
+        }
+    }
 }
 
-fn run_command(cmd: &str)  -> Result<(), Box<dyn Error>> {
+fn run_command(cmd: &str) -> Result<(), Box<dyn Error>> {
     let mut editor = BufferEditor {
-        buffer: Buffer::new()
+        buffer: Buffer::new(),
+        cursor_visible: true,
     };
+
     if cmd.starts_with("open") {
         run_game(
             &mut editor,
-            GameSettings::new()
-                .tick_duration(Duration::from_millis(25))
+            GameSettings::new().tick_duration(Duration::from_millis(500)),
         )?;
     } else {
         println!("Command not recognised!");
@@ -140,30 +164,27 @@ fn run_command(cmd: &str)  -> Result<(), Box<dyn Error>> {
 }
 
 use rustyline::error::ReadlineError;
-use rustyline::DefaultEditor;
+use rustyline::Editor;
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     println!("Welcome to BuffeRS. ");
 
-    let mut rl = DefaultEditor::new()?;
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new()?;
     loop {
         let readline = rl.readline(">> ");
         match readline {
             Ok(line) => {
                 run_command(&line)?;
-                let _ = rl.add_history_entry(line.as_str());
-            },
-            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
-                break
-            },
+                rl.add_history_entry(line.as_str());
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
             Err(err) => {
                 println!("Error: {:?}", err);
-                break
+                break;
             }
         }
     }
-
 
     Ok(())
 }
